77c31a1e103b45183c5bcc22a7afaa9fff38e9b4
mineninja77777 <thomasalexandersmith933@gmail.com>
2026-07-07 18:57:49 +0100

    restructure files

---
+++ src/engine/game_manager.py
@@ -0,0 +1,26 @@
+from __future__ import annotations # woah time travel
+
+from engine.base import Action_Type, Attack_Type
+from engine.action import Action, Attack, Effect
+from engine.entity import Entity
+from engine.encounter_manager import Encounter
+
+class GameManager:
+    _instance: GameManager | None = None
+
+    difficulty: int # the difficulty
+
+    def __init__(self):
+        self.players = []
+        self.difficulty = int(input("Difficulty: (1/2/3)")) # placeholder
+
+        
+
+    def main(self):
+        pass
+
+    @classmethod
+    def instance(cls) -> GameManager:
+        if cls._instance is None:
+            cls._instance = GameManager()
+        return cls._instance
---
+++ src/test.py
@@ -0,0 +1,34 @@
+# testing entity
+
+import inspect
+
+import engine.action as action
+import engine.base as base
+import engine.entity as entity
+import engine.encounter_manager as encounter_manager
+import engine.game_manager as game_manager
+import instances.effects
+import instances.entities
+import instances.others
+import instances.spells
+import instances.weapons
+
+# stats = StatBlock(100,0,50,1,{})
+# for attack_type in Attack_Type:
+#     stats.resistances[attack_type] = 1
+
+# entity1 = Entity("1", 1, True, stats, [])
+# entity2 = Entity("2", 1, False, stats, [])
+
+# entity1.execute_action(spells.Rot(1), [entity2], [entity2])
+
+# while not len(entity2.active_effects) == 0:
+#     entity2.tick_effects()
+#     print(entity2.hp)
+
+# entity2.execute_action(others.Rest(1), [], [])
+# while not len(entity2.active_effects) == 0:
+#     entity2.tick_effects()
+#     print(entity2.hp)
+
+# print(inspect.getmembers(instances.entities))
